374c6cd999f36dd8ba85c56574bdba112bea0a8c
regisf <regis.floret@gmail.com>
2014-01-21 20:29:33 +0400

    Oops

--- private_settings.py
+++
@@ -1,8 +0,0 @@
-__author__ = 'Regis FLORET'
-
-DB_NAME = "pythonregex"
-SECRET_KEY = "f2j!e0-&m6%=zmwa7a&08f4(94jd*ev3jui&r9g7nzy9t5+qb8"
-DEFAULT_EMAIL = "no-reply@pythonregex.com"
-RECAPTCHA_PUBLIC_KEY  = '6LcF_-kSAAAAACs_I6kyvyTPhECAg_QoZfDK4POy'
-RECAPTCHA_PRIVATE_KEY = '6LcF_-kSAAAAAPrlZYIreLG81fU79RW72u8jnC9k'
-RECAPTCHA_URL = 'http://www.google.com/recaptcha/api/verify'
