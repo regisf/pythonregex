d8c23280a4c674c5707bbaad1e8ba2b3860430c6
regisf <regisfloret@gmail.com>
2014-05-11 10:40:02 +0400

    Testing with tornado (which is not as simple as I thought)

---
+++ tests/test.py
@@ -0,0 +1,19 @@
+"""
+Test suite for the application
+
+Using the test suite in a shell or console:
+    python tests/test.py
+"""
+
+__author__ = 'Régis FLORET'
+
+
+import unittest
+
+
+class TestEmailModel(unittest):
+    def test_get_template(self):
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main()
