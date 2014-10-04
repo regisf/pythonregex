2029c9cb8cb393d153f1559bf4a4fa30b321d172
regisf <regisfloret@gmail.com>
2014-10-04 19:42:12 +0400

    Remove old javascripts

--- assets/js/appli/messagedialog.js
+++
@@ -1,7 +0,0 @@
-var MessageDialog = Class.extend({
-    init: function(title, msg) {
-        $("#messagedialog").find('[data-content]').html(msg);
-        $("#messagedialog").find('[data-title]').html(title);
-        new $.UIkit.modal.Modal("#messagedialog").show();
-    }
-});
