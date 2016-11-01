3f1cb6873e7b6fbcab336823cb7819a4edd931b6
regisf <regisfloret@gmail.com>
2016-11-01 20:58:55 +0100

    Better install script

--- bin/install.sh
+++ bin/install.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 # Install Python Virtual env only on non docker machine
-if [ "$1" != "--docker" ]; then
+if [ "$1" != "--docker" ];
+then
     # Install virtual env
     printf "Installing python 3 virtual environment\n"
     python3 -m venv env > /dev/null 2>&1
@@ -13,6 +14,20 @@ pip install --upgrade pip > /dev/null 2>&1
 
 printf "Installing requirements\n"
 pip install -r requirements.txt > /dev/null 2>&1
+
+printf "Ensure MongoDB\n"
+IS_CONFIGURED=`mongo pythonregex --quiet --eval "db.configuration.find().length()" > /dev/null 2>&1`
+if [ $? != 0 ];
+then
+    printf "\033[0;31mCan't connect to mongodb. Is it online?\033[0m\n"
+    exit 1
+fi
+
+if [ "$IS_CONFIGURED" = "0" ];
+then
+    SECRET=`date +%s | shasum -a 256 | base64 | head -c 32`
+    echo ${SECRET}
+fi
 
 printf "Done\n"
 
