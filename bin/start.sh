c274365a85fbeae652b50403ddc823109fc167fc
regisf <regisfloret@gmail.com>
2017-08-16 01:41:52 +0200

    Refactorize and add docker for easy deployment

--- bin/start.sh
+++ bin/start.sh
@@ -1,7 +1,7 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 printf "Starting PythonRegex application in \033[1m"
 if [ "$1" = "--dev" ]; then printf "DEVELOPER"; else printf "PRODUCTION"; fi; printf "\033[0m mode\n"
 
 cd Application
-python3 main.py $1
+python main.py $1
