8991cbd2e4bbd7c8ac7bc08611b03e8b1c4708b6
regisf <regisfloret@gmail.com>
2019-04-02 19:15:54 +0200

    Remove management scripts

--- bin/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-. ./bin/tests.sh
-
-docker build $1 -t pythonregex/web .
--- bin/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-printf "Starting PythonRegex application in \033[1m"
-if [ "$1" = "--dev" ]; then printf "DEVELOPER"; else printf "PRODUCTION"; fi; printf "\033[0m mode\n"
-
-cd Application
-python main.py $1
